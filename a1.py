6adae1b698a8f8bdf0670bc9127e8e3fb56912c8
Chris Yang <cyang@deltalearns.ca>
2021-02-08 14:52:09 -0800

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,4 +1,9 @@
 #! python3
+# There are two variables that have assigned values for x and y 
+# Display the sum of the two numbers (use addition) 
+# The answer should be calculated and stored in a variable called "answer" (1 points)
+
+
 
 x = 10
 y = 4
